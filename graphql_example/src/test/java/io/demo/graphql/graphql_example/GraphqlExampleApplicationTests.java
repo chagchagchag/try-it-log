91d2d1a7a7df24b97dc498db4cf70e0b103688e4
chagchagchag <chagchagchag.dev@gmail.com>
2024-06-05 23:51:27 +0900

    first status

---
+++ graphql_example/src/test/java/io/demo/graphql/graphql_example/GraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.demo.graphql.graphql_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
