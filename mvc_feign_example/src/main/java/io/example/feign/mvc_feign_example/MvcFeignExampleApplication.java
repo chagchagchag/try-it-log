63f1e309a7449e3f08f978ca2c9eee1b7f3636dd
chagchagchag <chagchagchag.dev@gmail.com>
2024-07-14 17:07:53 +0900

    Feat : open feign (first commit)

---
+++ mvc_feign_example/src/main/java/io/example/feign/mvc_feign_example/MvcFeignExampleApplication.java
@@ -0,0 +1,13 @@
+package io.example.feign.mvc_feign_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcFeignExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcFeignExampleApplication.class, args);
+	}
+
+}
---
+++ mvc_feign_example/src/test/java/io/example/feign/mvc_feign_example/MvcFeignExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.example.feign.mvc_feign_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcFeignExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
